aa908b9c482d94f97cb002d7e1519c0dbe7296bc
robgaston1 <robcoach0221@gmail.com>
2017-08-01 11:28:26 -0400

    move generateLife into separate function

--- app/app.jsx
+++ app/app.jsx
@@ -1,6 +1,14 @@
-import React, {Component} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import Game from './components/Game';
+import Game from './containers/Game';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
 
-ReactDOM.render(<Game/>, document.getElementById('app')
+const store = configureStore();
+
+ReactDOM.render(
+	<Provider store={store}>
+		<Game/>
+	</Provider>,
+	document.getElementById('app')
 	);
